3565d6fd693c9086dc52e03bfd7ebfd63d1bb6c9
Guan Xiaoyu <guanxy@me.com>
2017-03-31 18:30:48 +0800

    :lipstick: 增加 favicon

--- app.py
+++ app.py
@@ -67,5 +67,7 @@ def index():
     return redirect(url_for('todos.show'))
 
 @app.route('/robots.txt')
+@app.route('/favicon.svg')
+@app.route('/favicon.ico')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
